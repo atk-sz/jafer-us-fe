dc350e675ecb6ec86a24c789037b99d5b1922a80
sz-310 <syed.zaid@kerv.com>
2024-05-20 15:50:44 +0530

    bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./views/Home";
+import { Home, Login } from "./views";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
