7e422df6e26276533737eb87d5762483212ba5eb
sz-310 <syed.zaid@kerv.com>
2024-05-21 16:15:26 +0530

    updated redux setup & removed unwanted packages & code

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,22 +1,8 @@
 import axios from "axios";
-// js-cookie is to read/write server cookie which cannot be accessed in the frontend
-import Cookies from "js-cookie";
+
 // Set the base url of the backend api
 const BASE_URL = process.env.REACT_APP_BACKEND_API;
 // Create api with base url
 const api = axios.create({ baseURL: BASE_URL });
-
-/*
-Before making any request, check for token saved in the browser cookie,
-if theres a token in the cookie, add it to the request authorization
-header
-*/
-api.interceptors.request.use((req) => {
-  // If token exists, add to req auth header
-  if (Cookies.get("access")) {
-    req.headers.authorization = `Bearer ${Cookies.get("access")}`;
-  }
-  return req;
-});
 
 export default api;
--- src/redux/slices/auth.ts
+++ src/redux/slices/auth.ts
@@ -10,11 +10,11 @@ const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setCredentials: (state: any, action: any) => {
+    login: (state: any, action: any) => {
       state.userInfo = action.payload;
       localStorage.setItem('userInfo', JSON.stringify(action.payload));
     },
-    logout: (state: any, action: any) => {
+    logout: (state: any) => {
       state.userInfo = null;
       localStorage.removeItem('userInfo');
       // localStorage.clear();
@@ -22,6 +22,6 @@ const authSlice = createSlice({
   },
 });
 
-export const { setCredentials, logout } = authSlice.actions;
+export const { login, logout } = authSlice.actions;
 
 export default authSlice.reducer;
