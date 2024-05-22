5bb810bb53106b2100db246b479f09edd2d59254
sz-310 <syed.zaid@kerv.com>
2024-05-22 15:37:01 +0530

    minor req change

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { loggedInTestApi, logoutApi } from "../api/auth";
 import { useDispatch } from "react-redux";
 import { logout } from "../redux/slices/auth";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -32,9 +33,17 @@ const Home = () => {
     })();
   }, []);
   return (
-    <div>
+    <div
+      style={{
+        margin: "5%",
+      }}
+    >
       Home
       <button onClick={onClickLogout}>logout</button>
+      <div style={{ margin: "3%", width: "100%", display: "flex", gap: "10px" }}>
+        <Link to="/login">login</Link>
+        <Link to="/register">register</Link>
+      </div>
     </div>
   );
 };
